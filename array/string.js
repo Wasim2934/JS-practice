000f0ef5ceddd32175650be2df8dfa0355e2a3c6
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-08-12 00:03:32 +0530

    string initiated

---
+++ array/string.js
@@ -0,0 +1,23 @@
+// 1. Reverse a String
+// Input: "hello"
+// Output: "olleh"
+
+let str = "hello";
+
+// *************
+// let result = str.split('').reverse().join('') 
+// console.log(result);
+
+// ************
+// let result = ''
+
+// for (let i = str.length-1; i >= 0; i--) {
+//     result += str[i]
+// }
+
+// console.log(result);
+
+
+
+
+
