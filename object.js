d686703b8e80a27832660e6b33684b40cc5a892d
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-08-20 23:37:11 +0530

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+// 1. Access Object Properties
+const user = {
+  name: "Rahul",
+  age: 24,
+  city: "Delhi"
+};
+
+let {name, age, city} = user
+
+console.log(name);
+console.log(age);
+console.log(city);
+
