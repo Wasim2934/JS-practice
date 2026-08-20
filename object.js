7466522e7f4e09398a4de7a3f9f43e33e04a57fd
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-08-21 00:54:48 +0530

    more on object

--- object.js
+++ object.js
@@ -1,13 +1,82 @@
 // 1. Access Object Properties
-const user = {
-  name: "Rahul",
-  age: 24,
-  city: "Delhi"
-};
+// const user = {
+//   name: "Rahul",
+//   age: 24,
+//   city: "Delhi"
+// };
 
-let {name, age, city} = user
+// let {name, age, city} = user
 
-console.log(name);
-console.log(age);
-console.log(city);
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+// ----------------------------------
+
+// 2. Add a New Property
+// Add isAdmin: true and print the complete object.
+
+// const user = {
+//   name: "Rahul",
+//   age: 24
+// };
+
+// user.isAdmin = true
+
+// console.log(user);
+
+// -----------------------------------
+
+// 3. Update a Property
+// Change age to 25.
+
+// const user = {
+//   name: "Rahul",
+//   age: 24,
+//   city: "Delhi"
+// };
+
+// user.age = 25
+
+// console.log(user);
+
+// ----------------------------------
+
+// 4. Delete a Property
+// Delete the city property.
+
+// const user = {
+//   name: "Rahul",
+//   age: 24,
+//   city: "Delhi"
+// };
+
+// delete user.city
+
+// console.log(user);
+
+// ----------------------------------
+
+
+// 5. Check if Property Exists
+// Check whether email exists in the object.
+
+// const user = {
+//   name: "Rahul",
+//   age: 24,
+//   city: "Delhi"
+// };
+
+// if (user.email) {
+//     console.log('email exist');
+// } else console.log('email does not exist');
+
+
+
+
+
+
+
+
+
 
